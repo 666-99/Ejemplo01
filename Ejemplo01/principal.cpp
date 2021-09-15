0afcdf61f3b18172939090cdd4c84948b03fa56e
José Armando Vedia Rojas <969.rojas@gmail.com>
2021-09-15 00:18:02 -0700

    Agregar archivos de proyecto.

---
+++ Ejemplo01/principal.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Mi nombre es José Armando Vedia Rojas";
+	cout << "Mi cedula de identidad es 13883789";
+	cout << "Naci el 16  de enero del 2000";
+
+		return 0; 
+}
